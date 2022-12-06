23bf0c44a8872bc5e36d78302fa875eda9b2ced2
Victor Silva <vitor1908@gmail.com>
2022-12-06 12:47:55 -0300

    feat: create transactions table component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,51 @@
 import { Header } from '../../components/Header/index'
 import { Summary } from '../../components/Summary'
+import {
+  TransactionsContainer,
+  TransactionsTable,
+  PriceHighlight,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width='50%'>Desenvolvimento de sites</td>
+              <td>
+                <PriceHighlight variant='income'>R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width='50%'>Carne de ouro</td>
+              <td>
+                <PriceHighlight variant='outcome'>-R$ 10.000,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+            <tr>
+              <td width='50%'>Desenvolvimento de sites</td>
+              <td>R$ 12.000,00</td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width='50%'>Desenvolvimento de sites</td>
+              <td>R$ 12.000,00</td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
